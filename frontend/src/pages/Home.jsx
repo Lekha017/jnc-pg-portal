757d4bde7601a16105734febe1ada41e13a82dfc
Roshni Khan <roshnikhan1602@gmail.com>
2026-07-29 11:55:50 +0530

    Cleaned Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,11 +3,8 @@ import Navbar from "../components/layout/Navbar";
 import HeroSlider from "../components/home/HeroSlider";
 import AboutPGCentre from "../components/home/AboutPGCentre";
 import PrincipalMessage from "../components/home/PrincipalMessage";
-<<<<<<< HEAD
 import EventsAnnouncements from "../components/home/EventsAnnouncements";
-=======
 import Footer from "../components/layout/Footer";
->>>>>>> faculty-module
 
 function Home() {
   return (
@@ -16,12 +13,10 @@ function Home() {
       <Navbar />
       <HeroSlider />
       <AboutPGCentre />
-<<<<<<< HEAD
 
       {/* Principal Message + Events & Announcements */}
       <section className="bg-white py-12">
         <div className="max-w-7xl mx-auto px-6 lg:px-8 flex flex-col lg:flex-row items-start gap-10">
-
           {/* Left Side */}
           <div className="w-full lg:w-[500px] flex-shrink-0">
             <PrincipalMessage />
@@ -31,13 +26,10 @@ function Home() {
           <div className="flex-1">
             <EventsAnnouncements />
           </div>
-
         </div>
       </section>
-=======
-      <PrincipalMessage />
+
       <Footer />
->>>>>>> faculty-module
     </>
   );
 }
